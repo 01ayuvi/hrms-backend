1748a51f7220bd4777e8047153da76f6d8ead18c
Ayuvi Chaudhary <ayuvi.chaudhary@gmail.com>
2026-06-18 21:57:43 +0530

    Phase 5: Complete department CRUD APIs

--- app/departments/router.py
+++ app/departments/router.py
@@ -30,3 +30,46 @@ def create_department(
     db.refresh(department)
 
     return department
+
+@router.get("/")
+def get_departments(
+    db: Session = Depends(get_db)
+):
+
+    departments = db.query(
+        Department
+    ).all()
+
+    return departments
+@router.put("/{department_id}")
+def update_department(
+    department_id: int,
+    request: DepartmentUpdate,
+    db: Session = Depends(get_db)
+):
+
+    department = db.query(
+        Department
+    ).filter(
+        Department.id == department_id
+    ).first()
+
+    if not department:
+        raise HTTPException(
+            status_code=404,
+            detail="Department not found"
+        )
+
+    department.department_name = (
+        request.department_name
+    )
+
+    department.department_code = (
+        request.department_code
+    )
+
+    db.commit()
+
+    db.refresh(department)
+
+    return department
