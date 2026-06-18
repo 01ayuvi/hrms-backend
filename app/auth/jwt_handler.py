d86aab8546c1e8fc78229304161230f98bea543f
Ayuvi Chaudhary <ayuvi.chaudhary@gmail.com>
2026-06-18 15:31:32 +0530

    Phase 3.5: Move configuration to environment variables

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
 from jose import jwt
 
-SECRET_KEY = "hrms-secret-key"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
